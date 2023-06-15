92028c9aaee185e8675af139c054fc2d50d97937
Lycoris <zac7e4@outlook.com>
2023-06-15 18:09:20 -0400

    Add SqlHelper

---
+++ Appraisal_System/Utility/SqlHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appraisal_System.Utility
+{
+    public class SqlHelper
+    {
+        public static string Constr { get; set; }
+
+        public static DataTable ExecuteTable(string cmdText)
+        {
+            using (SqlConnection conn = new SqlConnection(Constr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                SqlDataAdapter sda = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                return ds.Tables[0];
+            }
+        }
+        
+        public static int ExecuteNonQuery(string cmdText)
+        {
+            using (SqlConnection conn = new SqlConnection(Constr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                int rows = cmd.ExecuteNonQuery();
+                if (rows <= 0) throw new Exception("DataSet cmd fails.");
+                return rows;
+            }
+        }
+    }
+}
