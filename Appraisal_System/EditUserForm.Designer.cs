7b6ea33d8c3e528eaacab567bfe395fe0734fa91
Lycoris <zac7e4@outlook.com>
2023-07-17 14:36:40 -0400

    Add EditUserForm

---
+++ Appraisal_System/EditUserForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Appraisal_System
+{
+    partial class EditUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chxSuspended = new System.Windows.Forms.CheckBox();
+            this.cbxSex = new System.Windows.Forms.ComboBox();
+            this.cbxIdentity = new System.Windows.Forms.ComboBox();
+            this.tbxUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.chxSuspended);
+            this.groupBox1.Controls.Add(this.cbxSex);
+            this.groupBox1.Controls.Add(this.cbxIdentity);
+            this.groupBox1.Controls.Add(this.tbxUserName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1356, 125);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Information";
+            // 
+            // chxSuspended
+            // 
+            this.chxSuspended.AutoSize = true;
+            this.chxSuspended.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chxSuspended.Location = new System.Drawing.Point(1162, 61);
+            this.chxSuspended.Name = "chxSuspended";
+            this.chxSuspended.Size = new System.Drawing.Size(163, 33);
+            this.chxSuspended.TabIndex = 6;
+            this.chxSuspended.Text = "Suspended";
+            this.chxSuspended.UseVisualStyleBackColor = true;
+            // 
+            // cbxSex
+            // 
+            this.cbxSex.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxSex.FormattingEnabled = true;
+            this.cbxSex.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.cbxSex.Location = new System.Drawing.Point(1008, 59);
+            this.cbxSex.Name = "cbxSex";
+            this.cbxSex.Size = new System.Drawing.Size(121, 37);
+            this.cbxSex.TabIndex = 5;
+            this.cbxSex.Text = "Male";
+            // 
+            // cbxIdentity
+            // 
+            this.cbxIdentity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxIdentity.FormattingEnabled = true;
+            this.cbxIdentity.Location = new System.Drawing.Point(476, 56);
+            this.cbxIdentity.Name = "cbxIdentity";
+            this.cbxIdentity.Size = new System.Drawing.Size(447, 37);
+            this.cbxIdentity.TabIndex = 4;
+            // 
+            // tbxUserName
+            // 
+            this.tbxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxUserName.Location = new System.Drawing.Point(170, 59);
+            this.tbxUserName.Name = "tbxUserName";
+            this.tbxUserName.Size = new System.Drawing.Size(183, 35);
+            this.tbxUserName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(948, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sex";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(381, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Identity";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(1041, 143);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(154, 59);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(1214, 143);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(154, 59);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // EditUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1380, 213);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "EditUserForm";
+            this.Text = "EditUserForm";
+            this.Load += new System.EventHandler(this.EditUserForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chxSuspended;
+        private System.Windows.Forms.ComboBox cbxSex;
+        private System.Windows.Forms.ComboBox cbxIdentity;
+        private System.Windows.Forms.TextBox tbxUserName;
+    }
+}
---
+++ Appraisal_System/EditUserForm.cs
@@ -0,0 +1,30 @@
+﻿using Appraisal_System.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appraisal_System
+{
+    public partial class EditUserForm : Form
+    {
+        public EditUserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EditUserForm_Load(object sender, EventArgs e)
+        {
+            List<AppraisalBases> appraisalBases = new List<AppraisalBases>();
+            appraisalBases = AppraisalBases.ListAll();
+            cbxIdentity.DataSource = appraisalBases;
+            cbxIdentity.DisplayMember = "BaseType";
+            cbxIdentity.ValueMember = "Id";
+        }
+    }
+}
